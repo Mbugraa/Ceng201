7275451261d82aba5432381cf4e45e2f7bfe42eb
Mustafa Buğrahan Önder <bugrahanonder@gmail.com>
2025-12-28 14:36:23 +0300

    Fibonacci factorial

---
+++ src/Fibo/RecursiveCount.java
@@ -0,0 +1,15 @@
+package Fibo;
+
+import java.io.FileOutputStream;
+
+public class RecursiveCount {
+
+    public static void main(String[] args) {
+
+        int sayilar[]= {3, 6, 7, 8};
+        FibonacciRecursive fb=new FibonacciRecursive();
+
+        System.out.println( fb.fibCount(sayilar,0));
+
+    }
+}
