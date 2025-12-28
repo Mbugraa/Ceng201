c382d93e88eaeca61afcd744938ac24fc0c7cebd
Mustafa Buğrahan Önder <bugrahanonder@gmail.com>
2025-12-28 14:14:05 +0300

    l6

---
+++ src/Stacks/Test.java
@@ -0,0 +1,21 @@
+package Stacks;
+
+public class Test {
+
+
+    public static void main(String[] args) {
+        MyStack stack = new MyStack();
+
+        stack.push(10);
+        stack.push(20);
+        stack.push(30);
+        stack.push(40);
+        stack.push(50);
+
+        stack.display();
+        System.out.println("Stack boyutu: " + stack.getSize());
+
+        stack.removeMiddle();
+        stack.display();
+    }
+}
